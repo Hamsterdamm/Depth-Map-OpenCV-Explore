0086aa2aad2137785e3329c4e306579c97714168
DESKTOP-SCVHRLK\Denis <andreev.93@gmail.com>
2017-04-23 00:13:13 +0300

    wls

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
+#include "opencv2/calib3d.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/core/utility.hpp"
+#include "opencv2/ximgproc/disparity_filter.hpp"
 #include <iostream>
-#include <opencv2\calib3d.hpp>
-#include <opencv\cv.h>
-#include <opencv2\imgcodecs.hpp>
-#include <opencv2\highgui.hpp>
+#include <string>
 
 #include <stdio.h>
 #include <stdarg.h>
 
 using namespace cv;
+using namespace cv::ximgproc;
+using namespace std;
 
 const char *windowDisparity = "Disparity";
 
@@ -19,10 +24,21 @@ int main()
 	cv::Mat imgLeft = imread(image_Name_1, cv::IMREAD_GRAYSCALE);
 	cv::Mat imgRight = imread(image_Name_2, cv::IMREAD_GRAYSCALE);
 
+	double lambda = 8000.0;
+	double sigma = 1.5;
+
+	Ptr<DisparityWLSFilter> wls_filter;
+
 
 	//-- And create the image in which we will save our disparities
-	Mat imgDisparity16S = Mat(imgLeft.rows, imgLeft.cols, CV_16S);
-	Mat imgDisparity8U = Mat(imgLeft.rows, imgLeft.cols, CV_8UC1);
+	Mat imgDisparity16SL = Mat(imgLeft.rows, imgLeft.cols, CV_16S);
+	Mat imgDisparity8UL = Mat(imgLeft.rows, imgLeft.cols, CV_8UC1);
+	Mat imgDisparity16SR = Mat(imgLeft.rows, imgLeft.cols, CV_16S);
+	Mat imgDisparity8UR = Mat(imgLeft.rows, imgLeft.cols, CV_8UC1);
+	Mat filtered_disp16S = Mat(imgLeft.rows, imgLeft.cols, CV_16S);
+	Mat filtered_disp8U = Mat(imgLeft.rows, imgLeft.cols, CV_8UC1);
+
+	
 
 	if (imgLeft.empty() || imgRight.empty())
 	{
@@ -33,26 +49,47 @@ int main()
 	int ndisparities = 16 * 5;   /**< Range of disparity */
 	int SADWindowSize = 9; /**< Size of the block window. Must be odd */
 
-	Ptr<StereoBM> sbm = StereoBM::create(ndisparities, SADWindowSize);
+	Ptr<StereoBM> sbmL = StereoBM::create(ndisparities, SADWindowSize);
+	Ptr<StereoBM> sbmR = StereoBM::create(ndisparities, SADWindowSize);
+	
+
+	Mat conf_map = Mat(imgLeft.rows, imgLeft.cols, CV_8U);
+	conf_map = Scalar(255);
 
 
 	//-- 3. Calculate the disparity image
-	sbm->compute(imgLeft, imgRight, imgDisparity16S);
+	sbmL->compute(imgLeft, imgRight, imgDisparity16SL);
+	sbmR->compute(imgRight, imgLeft, imgDisparity16SR);
+
+	//filter
+
+	wls_filter = createDisparityWLSFilter(sbmL);
+	wls_filter->setLambda(lambda);
+	wls_filter->setSigmaColor(sigma);
+	wls_filter->filter(imgDisparity16SL, imgLeft, filtered_disp16S, imgDisparity16SR);
+	//conf_map = wls_filter->getConfidenceMap();
 
 	//-- Check its extreme values
 	double minVal; double maxVal;
 
-	minMaxLoc(imgDisparity16S, &minVal, &maxVal);
+	minMaxLoc(imgDisparity16SL, &minVal, &maxVal);
 
 	//-- 4. Display it as a CV_8UC1 image
-	imgDisparity16S.convertTo(imgDisparity8U, CV_8UC1, 255 / (maxVal - minVal));
+	imgDisparity16SL.convertTo(imgDisparity8UL, CV_8UC1, 255 / (maxVal - minVal));
+	imgDisparity16SR.convertTo(imgDisparity8UR, CV_8UC1, 255 / (maxVal - minVal));
+
+	minMaxLoc(filtered_disp16S, &minVal, &maxVal);
+
+	//-- 4. Display it as a CV_8UC1 image
+	filtered_disp16S.convertTo(filtered_disp8U, CV_8UC1, 255 / (maxVal - minVal));
 
 //	namedWindow(windowDisparity, WINDOW_NORMAL);
 
 //	cv::ShowManyImages("Images", 2, img1, img2);
 	imshow("imgLeft", imgLeft);
 	imshow("imgRight", imgRight);
-	imshow("disp", imgDisparity8U);
+	imshow("disp", imgDisparity8UL);
+	imshow("filtered disp", imgDisparity8UR);
 
 	waitKey(0);
 
